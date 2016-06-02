e269f2777edcf78b5d2fb7423b5e93abedb45adc
flying-pig <chenhuaying@sina.cn>
2016-06-02 19:12:14 +0800

    udpate C++ example

--- example.cc
+++ example.cc
@@ -20,6 +20,16 @@ int bar() {
 	return 1;
 }
 
+class MyClass {
+public:
+	MyClass() { std::cout << "construct" << std::endl; }
+	~MyClass() { std::cout << "destruct" << std::endl; }
+
+	void doTask() {
+		std::cout << "do Task now" << std::endl;
+	}
+};
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
@@ -27,6 +37,9 @@ int main(int argc, char *argv[])
 
 	//std::cout << "Program: " << argv[0] << std::endl;
 	printf("foo=%d bar=%d\n", foo(), bar());
+
+	MyClass a;
+	a.doTask();
 
 	return 0;
 }
